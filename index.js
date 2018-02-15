9ac47fd604cac6e94d547990621ae5295c207f78
sayansardar91 <sardar.sayan91@gmail.com>
2018-02-15 14:12:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
+const http = require('http')
 const port = 3000
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-})
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
 
-app.listen(port, (err) => {
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
